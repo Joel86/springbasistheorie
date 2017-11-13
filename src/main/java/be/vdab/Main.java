7022a906d2071f36c096b386b2544ac3400effe6
Joel Vanherle <joel.vanherle@hotmail.com>
2017-11-13 10:54:43 +0100

    Setter injection

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -7,8 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 		try(ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("container.xml")) {
+			ClassA objectA = context.getBean(ClassA.class);
 			System.out.println(context.getBean(ClassA.class).getBoodschap());
 			System.out.println(context.getBean(InterfaceB.class).getBoodschap());
+			System.out.println(context.getBean(ClassA.class).getTelefoonNrHelpDesk());
+			System.out.println(objectA.getKoersenURL());
+			System.out.println(objectA.getDefaultTaalEnLand().getDisplayName());
+			System.out.println("Bestand bestaat: " + objectA.getImportData().exists());
+			System.out.println(objectA.getDefaultBladRichting());
 		}
 
 	}
